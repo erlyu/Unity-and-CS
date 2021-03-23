8bc4888eaa7a956ca7d9451f491c6b2f16d66f03
erlyu <46057691+erlyu@users.noreply.github.com>
2021-03-22 19:23:59 -0700

    Hello World Finished

---
+++ Hello World/Assets/HelloWorld.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start(){
+        print("Hello Eric, Welcome to the world of game development!");
+    }
+
+    // Update is called once per frame
+    void Update(){
+        
+    }
+}
